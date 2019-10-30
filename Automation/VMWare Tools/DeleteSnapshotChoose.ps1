78c04794db3d32708dd20081891aef4766318959
Benjamin Lucas <benjaminhmlucas@gmail.com>
2019-10-30 13:34:33 -0400

    Add files via upload

---
+++ Automation/VMWare Tools/DeleteSnapshotChoose.ps1
@@ -0,0 +1,213 @@
+﻿###########################################################
+#-- Script: DeleteSnapshotChoose.ps1
+#-- Created: 10/30/2019
+#-- Author: Ben Lucas 
+#-- Description: Connects to ESXi 6 or above host and allows for group deletion 
+#-- of snapshots and single deletion if snapshots number or order is different from 
+#-- a set server/domain controller. 
+#-- Must add values to lines 14-18
+#--  
+#-- History: Created in October 2019 
+#-- BL         1.0.0.0 10/30/2019
+###########################################################
+import-module vmware.vimautomation.core
+Add-Type -AssemblyName System.Windows.Forms
+
+$EsxServer = '<Enter ESX Server Name/IP>'
+$EsxUser = '<Enter username>'
+$ESXPassword = '<Enter Password>'
+$DomainControllerName = '<Enter Domain Controller Name>'
+$ServersToExclude = "<Name pattern for any VMs you don't want to snap>"
+
+#function region
+function connectVIServers{
+#Connect-VIServer -Server '<ESX Server Name/IP>' -user '<username>' -Password '<Password>'>>Add more to connect to more ESX servers
+Connect-VIServer -Server $EsxServer -user $EsxUser -Password $ESXPassword
+}
+
+function disconnectVIServers{
+#Connect-VIServer -Server '<ESX Server Name/IP>' -Confirm:$false >>corresponds to multiple additions for connectVIServer
+Connect-VIServer -Server $EsxServer -Confirm:$false
+}
+
+function listVMSnaps{
+    Param(
+    [Parameter(Mandatory=$true)]
+    [string]$VMToGetSnapListFrom
+    )
+    $ListNumberCounter = 1
+    $Snapshots = get-snapshot -vm $VMToGetSnapListFrom
+    write-host ("Choose a Snapshot to Delete:")
+    forEach ($Snap in $Snapshots){
+        write-host ("("+ $ListNumberCounter++ +"): " + $Snap.name) -ForegroundColor Green
+    
+    }     
+}
+
+function quitOrContinueOption{
+    Write-Host "`nType: " -nonewline
+    Write-Host "'q' + [Enter]" -foregroundcolor red -nonewline
+    Write-Host " to QUIT" 
+    Write-Host "Type: " -nonewline
+    Write-Host "[Enter]" -foregroundcolor green -nonewline
+    $response = Read-Host " to CONTINUE" 
+    if($response.ToString().ToLower() -eq 'q'){
+        disconnectVIServers
+        exit
+    }
+}
+
+function quitSkipOrContinueOption{
+    Write-Host "`nType: " -nonewline
+    Write-Host "'q' + [Enter]" -foregroundcolor red -nonewline
+    Write-Host " to QUIT" 
+    Write-Host "Type: " -nonewline
+    Write-Host "'s'" -foregroundcolor green -nonewline
+    Write-Host "Type: " -nonewline
+    Write-Host "[Enter]" -foregroundcolor green -nonewline
+    $response = Read-Host " to CONTINUE" 
+    if($response.ToString().ToLower() -eq 'q'){
+        disconnectVIServers
+        exit
+    }
+    if($response.ToString().ToLower() -eq 's'){
+        $skipSnap = $true
+    }
+}
+#Variable Region
+$DomainController = get-vm | Where-Object {$_.Name -like ('*'+$DomainControllerName+'*')}
+$DomainControllerSnaps = get-snapshot -vm $DomainController #snapshots should match Domain Controller's snapshots
+$ListNumberCounter = 1 #written to host number signifying the number of the snapshot for that VM
+$VMs = Get-VM | Where-Object {$_.name -notlike ('*'+$ServersToExclude+'*')} #All VMs
+$VMsWithDifferingSnaps = @() #list of snaps with differing number from the average or snapshot names mismatched from the Domain Controller
+[bool]$SnapsAreDifferent = $false  
+$SnapToDeleteIndex = -1
+$skipSnap = $false
+
+#Execution Region
+connectVIServers
+#check snapshot differences
+forEach ($VM in $VMs){
+    $snapCounter = 0
+    $Snapshots = get-snapshot -vm $VM
+    #check number of snapshot differences from the Domain Controller
+    if($DomainControllerSnaps.Count -ne $Snapshots.Length){
+        $SnapsAreDifferent = $true
+        $VMsWithDifferingSnaps += ,$VM
+        $VMs = $VMs | Where-Object {$_.Name -ne $VM.Name}
+    } else {
+        #check snapshot with different names from the Domain Controller       
+        forEach ($Snap in $Snapshots){
+            if($Snap.Name -ne ($DomainControllerSnaps.Get($snapCounter)).name){
+                $SnapsAreDifferent = $true
+                $VMsWithDifferingSnaps += ,$VM
+                $VMs = $VMs | Where-Object {$_.Name -ne $VM.Name}
+            }
+            $snapCounter = $snapCounter + 1
+        }        
+    }
+}
+
+#if there are differeng snapshots then 
+if($SnapsAreDifferent){
+    #inform user of snapshot differences
+    Write-Host "`nThe Snapshots on These VMs differ from the Domain Controller by name, amount or order." -foregroundcolor red
+    Write-Host "Best Practices dictate that you should have the same number of snaps with the same names for every VM." -foregroundcolor red
+    Write-Host "It may be a good idea to manually remove your snaps if this is not the case." -foregroundcolor red
+    Write-Host "If you would like to continue, you will have a chance to decide which snap to delete on differeing VMs later.`n" -foregroundcolor red
+
+    forEach ($VM in $VMsWithDifferingSnaps){
+        Write-Host ("$vm.Name") -foregroundcolor magenta
+    }
+    quitOrContinueOption
+}
+
+Write-Host "`n"
+#List Snapshots on VMs to delete
+listVMSnaps $DomainController
+
+while(($SnapToDeleteIndex -lt 0) -or ($SnapToDeleteIndex -ge $Snapshots.length) -or ($SnapToDeleteIndex -isnot [int])){
+    $SnapToDeleteIndex = Read-Host "`nPlease enter the index number of the snapshot you want to delete"
+    try{
+        $SnapToDeleteIndex = ($SnapToDeleteIndex-1)
+        if(($SnapToDeleteIndex -lt 0) -or ($SnapToDeleteIndex -ge $Snapshots.length)-or ($SnapToDeleteIndex -isnot [int])){
+            write-host "Please enter a number! Please pick one of the green numbers to the left of the snapshot names.`n" -foreground red
+            listVMSnaps $DomainController
+        }
+    }catch{
+         write-host "Incorrect snapshot number! Please pick one of the green numbers to the left of the snapshot names.`n" -foreground red
+         listVMSnaps $DomainController
+    }
+    
+
+}
+
+forEach ($VM in $VMs)
+{
+    write-host $VM.name -NoNewline -ForegroundColor green
+    write-host "-->> Snapshot that will be deleted: " -NoNewline
+    write-host $Snapshots.get(($SnapToDeleteIndex)) -ForegroundColor Red
+}
+
+$response = Read-Host "Are You Sure You want to delete these Snapshots? Please look at list, they will be gone forever!!` 
+-->>Type 'y' to delete these snapshots!!"
+
+if($response -eq 'y'){
+    forEach ($VM in $VMs){
+        $Snapshots = get-snapshot -vm $VM
+        Write-host ("Removing Snapshot: "+$Snapshots.get($SnapToDeleteIndex)+" from VM: "+$VM.Name) -ForegroundColor red
+        #Remove-Snapshot -Snapshot $Snapshots.get(0) -Confirm:$false
+    }
+
+}else{
+    Write-host "Operation Cancelled, Good day."
+    disconnectVIServers
+    exit
+}
+
+write-host "Now would you like to deal with the VMs with differing snapshots?"
+quitOrContinueOption
+
+forEach ($VM in $VMsWithDifferingSnaps){
+    Write-Host ($VM.Name + " Snapshots:")
+    listVMSnaps $VM
+    quitSkipOrContinueOption
+    $SnapToDeleteIndex = -1
+    if(!($skipSanp)){
+        while(($SnapToDeleteIndex -lt 0) -or ($SnapToDeleteIndex -ge $Snapshots.length) -or ($SnapToDeleteIndex -isnot [int])){
+            $SnapToDeleteIndex = Read-Host "`nPlease enter the index number of the snapshot you want to delete"
+            try{
+                $SnapToDeleteIndex = ($SnapToDeleteIndex-1)
+                if(($SnapToDeleteIndex -lt 0) -or ($SnapToDeleteIndex -ge $Snapshots.length)-or ($SnapToDeleteIndex -isnot [int])){
+                    write-host "Please enter a number! Please pick one of the green numbers to the left of the snapshot names.`n" -foreground red
+                    listVMSnaps $VM.Name
+                }
+            }catch{
+                 write-host "Incorrect snapshot number! Please pick one of the green numbers to the left of the snapshot names.`n" -foreground red
+                 listVMSnaps $VM.Name
+            }
+        }        
+    }
+    $skipSnap = $false
+    write-host $VM.name -NoNewline -ForegroundColor green
+    write-host "-->> Snapshot that will be deleted: " -NoNewline
+    write-host $Snapshots.get(($SnapToDeleteIndex)) -ForegroundColor Red
+    $response = Read-Host "Are You Sure You want to delete these Snapshots? Please look at list, they will be gone forever!!` 
+    -->>Type 'y' to delete these snapshots!!"
+
+    if($response -eq 'y'){
+        $Snapshots = get-snapshot -vm $VM
+        Write-host ("Removing Snapshot: "+$Snapshots.get($SnapToDeleteIndex)+" from VM: "+$VM.Name) -ForegroundColor red
+        #Remove-Snapshot -Snapshot $Snapshots.get(0) -Confirm:$false
+
+
+    }else{
+        Write-host "Operation Cancelled, moving to next VM"
+    }
+
+}
+
+disconnectVIServers
+
+Write-Host "Done-zo!"
+[System.Windows.Forms.MessageBox]::Show('Snapshot Deletion Complete!','All Done','OK','Info');
---
+++ Automation/VMWare Tools/SnapshotAllVMs.ps1
@@ -0,0 +1,85 @@
+﻿###########################################################
+#-- Script: SnapshotAllVMs.ps1
+#-- Created: 9/12/2019
+#-- Author: Ben Lucas 
+#-- Description: Snapshot all VMs for domain, ESX 6 or above
+#-- History: Created in August 2019 
+#-- BL         1.0.0.0 9/12/2019
+###########################################################
+
+#Preliminary steps
+#Load the VMWare PowerCLI SnapIn so that PowerCLI commandlets can be used
+
+import-module vmware.vimautomation.core
+Add-Type -AssemblyName System.Windows.Forms
+
+#Set Snapshot Name
+$nameCheck = 0
+while($nameCheck -ne 1){
+    [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') |
+    Out-Null
+    $SnapshotName = [Microsoft.VisualBasic.Interaction]::InputBox("Enter a Snapshot Name. To exit leave name empty & hit OK, or click Cancel","Snapshot","$snapName");
+    if($SnapshotName -eq ''){
+        exit
+    } else{
+        Write-Host "Snapshot will be named: $SnapshotName";
+        $nameCheckResponse = [System.Windows.Forms.MessageBox]::Show('Are you sure this is the right name? ' + $SnapshotName,'Name Check','OKCancel','Info');
+        switch ($nameCheckResponse){
+            'OK'{$nameCheck = 1};
+        }
+    }
+
+}
+Start-sleep -Seconds 5
+
+#Get Start time
+$ScriptStartTime = (Get-Date)
+$BackupDate = (Get-Date -format ddMMMyyyy)
+Write-Host The current time is $ScriptStartTime
+
+#Connect to an ESX host
+#Connect-VIServer -Server '<ESX Server Name/IP>' -user '<username>' -Password '<Password>'>>Add more to connect to more ESX servers
+Connect-VIServer -Server '<ESX Server Name/IP>' -user '<username>' -Password '<Password>'
+
+
+$VMs = Get-VM
+#Disconnect CD
+#run through the vm and disconnect the items in the CD Drives for each and set to NoMedia.
+Write-Host Disconnecting CD drives
+forEach ($VM in $VMs)
+{
+  $status = get-vm $VM | Get-CDDrive | Where-Object { $_.name -like "*1"} |Set-CDDrive -NoMedia -Confirm:$false -ErrorAction SilentlyContinue
+  $status = get-vm $VM | Get-CDDrive | Where-Object { $_.name -like "*2"} |Set-CDDrive -NoMedia -Confirm:$false -ErrorAction SilentlyContinue
+  $status = get-vm $VM | Get-CDDrive | Where-Object { $_.name -like "*3"} |Set-CDDrive -NoMedia -Confirm:$false -ErrorAction SilentlyContinue
+  $status = get-vm $VM | Get-CDDrive | Where-Object { $_.name -like "*4"} |Set-CDDrive -NoMedia -Confirm:$false -ErrorAction SilentlyContinue
+  $status = get-vm $VM | Get-CDDrive | Where-Object { $_.name -like "*5"} |Set-CDDrive -NoMedia -Confirm:$false -ErrorAction SilentlyContinue
+  $status = get-vm $VM | Get-CDDrive | Where-Object { $_.name -like "*6"} |Set-CDDrive -NoMedia -Confirm:$false -ErrorAction SilentlyContinue
+}
+# Get time, compute and display total time to this point
+$CurrentTime = (Get-Date)
+Write-Host "The current time is $CurrentTime"
+$TimeForScript = (New-TimeSpan -Start $ScriptStartTime -End $CurrentTime)
+Write-Host "The script has run for for a total time in HH:MM:SS of $TimeForScript"
+Write-Host "All CD that were connected should now be disconnected"
+Write-Host "CONTINUING"
+
+#Take Snapshots for each VM using input name from above
+$VMs = Get-VM
+forEach ($VM in $VMs)
+{
+  $Snapshots = get-snapshot -vm $VM
+  write-host "Here is the list of existing snapshots: $Snapshots"
+  Write-host "IGNORE any red that occurrs in this section"
+  New-Snapshot -VM $VM -Name $SnapshotName
+}
+# Get Finish time, compute and display total time script ran
+$ScriptFinishTime = (Get-Date)
+Write-Host "The current time is $ScriptFinishTime"
+$TimeForScript = (New-TimeSpan -Start $ScriptStartTime -End $ScriptFinishTime)
+Write-Host "The total time to prepare and perform snapshot orperation in HH:MM:SS was $TimeForScript"
+
+#Connect-VIServer -Server '<ESX Server Name/IP>' -Confirm:$false
+Connect-VIServer -Server '<ESX Server Name/IP>' -Confirm:$false
+
+[System.Windows.Forms.MessageBox]::Show('Snapshot Complete!','All Done','OK','Info');
+Start-sleep -seconds 10
