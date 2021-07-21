b3d7c4fc162add3279572ce1d77a9817e0f6b38c
Benjamin Lucas <benjaminhmlucas@gmail.com>
2021-07-21 14:05:18 -0400

    Add files via upload

--- Automation/Active Directory Tools/PasswordExpiration.ps1
+++ Automation/Active Directory Tools/PasswordExpiration.ps1
@@ -4,27 +4,24 @@
 #-- Author: Ben Lucas 
 #-- DESCRIPTION
 #--     Sets all accounts with formats that you choose (ex:???.* and ??.*) to expire or not depeneding on passed in variable. 
-#--     Sets account password to chosen sting line 16. Line 22 will allow you to filter by name patterns but needs to be modified 
+#--     Sets account password to chosen sting line 18. Line 19 will allow you to filter by name patterns but needs to be modified 
 #--     for specific name schema.
 #-- PARAMETER setPasswordExpirationTo
 #--     Type anything and hit [Enter] to set passwords to not expire, Type nothing and hit [Enter] to turn on password expiration.
 #-- History: Created in August 2019 
 #-- BL         1.0.0.0 10/30/2019
 ############################################################>
-param (
-    [Parameter(Mandatory=$true,
-    HelpMessage = "Type anything and hit [Enter] to set passwords to not expire, Type nothing and hit [Enter] to turn on password expiration.")]
-    [bool]$setPasswordExpirationTo
-)
-
+$global:setPasswordExpirationTo = $false
+$userAnswer = Read-Host("Type 'off' and hit [enter] to turn off expiration, just hit [enter] to skip this: ")
+if($userAnswer.Equals("off")) { $global:setPasswordExpirationTo = $true }
 Import-Module ActiveDirectory
-$DefaultPwd = ConvertTo-SecureString -String "<New Password to set" -AsPlainText -Force
+$NewPwdCred = Get-Credential -Message "Enter the new password in the password field below.  You can ignore the username." -UserName "IGNORE THIS FIELD"
 $AllUsers = Get-ADUser -Filter * | Where-Object {$_.name -like '??.*' -or $_.name -like '???.*'}
 $AllUsers = $AllUsers | Sort-Object -Property name
 #$user
 forEach($user in $Allusers){
-    Set-ADUser -Identity $user.DistinguishedName -PasswordNeverExpires $setPasswordExpirationTo 
-    Set-ADAccountPassword -Identity $user.DistinguishedName -NewPassword $DefaultPwd
-    Write-Host ($user.name + " Password Never Expires: $setPasswordExpirationTo")
+    Set-ADUser -Identity $user.DistinguishedName -PasswordNeverExpires $global:setPasswordExpirationTo
+    Set-ADAccountPassword -Identity $user.DistinguishedName -NewPassword $NewPwdCred.Password
+    Write-Host ($user.name + " Password Never Expires: $global:setPasswordExpirationTo")
     Write-Host ($user.name + " Password Reset")
 }
